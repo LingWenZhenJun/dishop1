e81cdb1c77b68476d680aa7728ae6c14bfc0f7e1
Your Name <you@example.com>
2025-07-25 11:37:27 +0300

    2

--- di/settings.py
+++ di/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dishop1.onrender.com', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
